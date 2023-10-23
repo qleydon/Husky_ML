5f4a0c336eaae0f56b6490eb001ac15216b0a7eb
qleydon <q.leydon@gmail.com>
2023-10-23 12:10:50 -0400

    not getting image correctly

---
+++ src/HUSKY_RL/Husky_RNDUtils.py
@@ -0,0 +1,324 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  2 11:57:33 2023
+
+@author: Breach
+"""
+
+import torch
+import numpy as np 
+import torch.nn as nn 
+from torch.optim.adam import Adam
+from src.HUSKY_RL.Husky_Networks import CustomNetwork
+import itertools
+
+
+class JSD(nn.Module):
+    def __init__(self):
+        super(JSD, self).__init__()
+        self.kl = nn.KLDivLoss(reduction='batchmean', log_target=True)
+
+    def forward(self, p: torch.tensor, q: torch.tensor):
+        p, q = p.view(-1, p.size(-1)), q.view(-1, q.size(-1))
+        m = (0.5 * (p + q)).log()
+        return 0.5 * (self.kl(p.log(), m) + self.kl(q.log(), m))
+
+class MAPE(nn.Module):
+    def __init__(self):
+        super(MAPE, self).__init__()
+        self.mae = nn.L1Loss()
+
+    def forward(self, target: torch.tensor, prediction: torch.tensor):
+        target, prediction = target.view(-1, target.size(-1)), prediction.view(-1, prediction.size(-1))
+        return (self.mae(target, prediction))/(torch.abs(target)+torch.finfo(torch.float32).eps)
+
+
+
+class RunningMeanStd(object):
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self,batch_size, epsilon=1e-4, shape=()):
+        self.mean = np.zeros(shape, 'float64')
+        self.var = np.ones(shape, 'float64')
+        self.count = epsilon
+        self.moving_avg_list=[]
+        self.batch_size=batch_size
+        
+    def update(self, x):
+        batch_mean, batch_std, batch_count = np.mean(x), np.std(x), self.batch_size#x.shape[0]
+        batch_var = np.square(batch_std)
+        self.update_from_moments(batch_mean, batch_var, batch_count)
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        delta = batch_mean - self.mean
+        tot_count = self.count + batch_count
+
+        new_mean = self.mean + delta * batch_count / tot_count
+        m_a = self.var * (self.count)
+        m_b = batch_var * (batch_count)
+        M2 = m_a + m_b + np.square(delta) * self.count * batch_count / (self.count + batch_count)
+        new_var = M2 / (self.count + batch_count)
+
+        new_count = batch_count + self.count
+
+        self.mean = new_mean
+        self.var = new_var
+        self.std=self.var**(1/2)
+        self.count = new_count
+        
+    def moving_norm(self,x):
+        x=(x-self.mean)/self.std
+        # x=x*.9999
+        return x
+
+
+class RNDTauSetter():
+    
+    
+    def __init__(self,
+                 input_dim,
+                 loss_type,
+                 initial_tau,
+                 output_dim=2000,
+                 learning_rate=.0003,
+                 proportion_of_exp_used_for_predictor_update=1,
+                 caution=True
+                 ):
+        
+        self.decay_rate= 0.5
+        self.increase_factor = 1
+        self.current_trace=0
+        self.max_error=1
+
+        self.lr=learning_rate
+        self.current_lr=learning_rate
+        self.input_dim=input_dim
+        self.output_dim=output_dim
+        self.loss = self._get_loss_function(loss_type)
+        self.initial_tau=initial_tau
+        self.current_tau=initial_tau
+        self._set_networks(input_dim)
+        self.caution=caution
+        self.proportion_of_exp_used_for_predictor_update=proportion_of_exp_used_for_predictor_update
+    
+        
+    def _get_loss_function(self,loss_type):
+        if loss_type == 'KL':
+            self.activation_type="softmax"
+            return torch.nn.KLDivLoss(reduction="none")
+        elif loss_type == 'MSE':
+            self.activation_type=None
+            return nn.MSELoss(reduction="none")
+        elif loss_type == 'MAE':
+            self.activation_type=None
+            return nn.L1Loss
+        elif loss_type == 'MAPE':
+            self.activation_type=None
+            return MAPE()
+        elif loss_type == 'JSD':
+            self.activation_type="softmax"
+            return JSD()
+        else:
+            raise ValueError(f"Unsupported loss function: {loss_type}")
+        
+        
+    def  _set_networks(self,inputdim,output_dim=2000):
+        self.target=CustomNetwork(None,True,inputdim,output_dim,"dense_net",self.activation_type)
+        self.predictor_g=CustomNetwork(None,True,inputdim,output_dim,"dense_net",self.activation_type)
+        self.predictor_l=CustomNetwork(None,True,inputdim,output_dim,"dense_net",self.activation_type)
+        self.predictor_g.load_state_dict(self.predictor_l.state_dict())
+        
+        self.predictor_g_opt=Adam(self.predictor_g.parameters(), lr=self.lr)
+        self.predictor_l_opt=Adam(self.predictor_l.parameters(), lr=self.lr)
+        
+    def calculate_errors(self,states_global,states_local):
+        with torch.no_grad():
+            y_global=self.target(states_global)  
+            y_local=self.target(states_local) 
+            
+            prediction_global=self.predictor_g(states_global)
+            prediction_local=self.predictor_l(states_local)
+            
+            
+            global_error =self.loss(y_global,prediction_global)
+            global_error=torch.mean(global_error)
+            
+            local_error =self.loss(y_local,prediction_local)
+            local_error=torch.mean(local_error)
+        return global_error,local_error
+        
+        
+        
+    def apply_mask(self,loss):
+        size=loss.size()
+        shape=np.asarray(size)
+        global_mask=torch.FloatTensor(*shape).uniform_(0, 1)
+        global_mask=(global_mask < self.proportion_of_exp_used_for_predictor_update).type(torch.float32)
+        loss=torch.sum(loss*global_mask)/torch.max(torch.sum(global_mask),torch.ones(1))
+        return loss
+
+
+
+    def update_rnd(self,states_global,states_local):
+        with torch.no_grad():
+            noisy_targets_global=self.target(states_global)  
+            noisy_targets_local=self.target(states_local) 
+        
+        global_prediction=self.predictor_g(states_global)
+        local_prediction=self.predictor_l(states_local)
+        
+
+        global_loss=self.loss(noisy_targets_global,global_prediction)#**(1/2)
+        local_loss=self.loss(noisy_targets_local,local_prediction)
+        
+        global_loss=self.apply_mask(global_loss)
+        local_loss=self.apply_mask(local_loss)
+        # print(local_loss.detach().numpy())
+        
+        local_loss=torch.mean(local_loss)
+        global_loss=torch.mean(global_loss)
+        
+        # print(local_loss.detach().numpy())
+        self.predictor_g_opt.zero_grad()
+        global_loss.backward()
+        self.predictor_g_opt.step()
+        
+        self.predictor_l_opt.zero_grad()
+        local_loss.backward()
+        self.predictor_l_opt.step()
+        
+        error=global_loss-local_loss
+        
+        if self.caution:
+            self.caution_lr(error)
+
+        return global_loss,local_loss,error
+    def step(self, error):
+      """
+      Pass in the current trace value and the error to compute the next trace value
+      :return: The new trace value
+      """
+      
+      error=torch.abs(error)
+      error=error.detach().numpy()
+      if self.max_error<error:
+          self.max_error=error
+      error=error/self.max_error
+      # print("this is the error "+str(error))
+      delta = -self.decay_rate * self.current_trace + self.increase_factor * error
+      self.current_trace=self.current_trace + delta
+      # print("this is the current trace "+ str(self.current_trace))
+    
+
+    def caution_lr(self,error):
+        self.step(error)
+        if self.current_trace > .5:
+            self.current_lr = (1-self.current_trace)*self.current_lr
+            self.predictor_g_opt.param_groups[0]['lr']=self.current_lr
+            self.predictor_l_opt.param_groups[0]['lr']=self.current_lr
+        else:
+            self.current_lr = self.lr
+            self.predictor_g_opt.param_groups[0]['lr']=self.current_lr
+            self.predictor_l_opt.param_groups[0]['lr']=self.current_lr
+
+
+    def norm_tau_setter_tf(self,Error_0,alpha,beta,current_error):
+        """
+        Parameters
+        ----------
+        T_0 : Initial Tau
+            Stable known Env Tau.
+        Error_0 : Initial Error
+            Firts calculated error of the RND agent .
+        alpha : Hyper Parameter
+            DESCRIPTION.
+        beta : Hyper Parameter
+            DESCRIPTION.
+        current_error : New Error
+            Error of the current predictor of RND.
+
+        Returns
+        -------
+        Tau.
+
+        """
+        num=2*self.initial_tau
+        denom=1+torch.pow(Error_0/(alpha*current_error), torch.log(Error_0))
+        Tau=num/denom+beta
+        return Tau
+        
+    
+    # def tau_setter_rv(self,base_tau,states_global,states_local,prior_shift,prior_tau):
+    #    global_error,local_error= self.calculate_error(states_global, states_local)
+    #    n_local,n_global=self.norm_error(local_error),self.norm_error(local_error)
+    #    env_shift=n_local-n_global
+    #    novelty_shift=(env_shift)*self.base_tau
+    #    tau=prior_tau+novelty_shift
+    #    return tau
+       
+       
+
+
+# learning_rates=[]
+
+# # # Example usage with convolutional layers:
+# # layer_params = [
+# #     ('linear',28,400,'relu'),
+# #     ('linear',400,350,'relu'),
+# #     ('linear',350,250,'relu'),
+# #     ('linear',250,100,'relu'),
+# #     ('linear',100,56,"softmax")
+# #     ]
+
+# input_data = torch.randn(64, 28) # Example input data (batch size: 64, channels: 3, height: 28, width: 28)
+# # net = CustomNetwork(layer_params)
+# # output = net(input_data)
+# # print(output.shape)  # This will print the shape of the output tensor after passing through all layers and activations.
+
+# # net =CustomNetwork(None,True,28,56,"dense_net",'softmax')
+# # CustomNetwork(None,True,28,56,"conv_net",'softmax')
+# # CustomNetwork(layer_params,False,None,None,None,None)
+# test_lr=[.0003,.5*.0003,.25*.0003]
+# for i in test_lr:
+#     tausetter=RNDTauSetter(28,"MAPE",.01,learning_rate=i)
+#     all_local=[]
+#     all_global=[]
+    
+#     for i in range(65):
+#        global_loss,local_loss= tausetter.update_rnd(input_data,input_data)
+#        global_loss=global_loss.detach().numpy()
+#        local_loss=local_loss.detach().numpy()
+#        all_local.append(local_loss)
+#     all_local=all_local/all_local[0]
+#     learning_rates.append(all_local)
+
+   
+
+# import matplotlib.pyplot as plt
+
+# def plot_three_lists(list1, list2, list3):
+#     """
+#     Plot three lists of data using Matplotlib.
+
+#     Parameters:
+#         list1 (list): The first list of data points.
+#         list2 (list): The second list of data points.
+#         list3 (list): The third list of data points.
+#     """
+#     if len(list1) != len(list2) or len(list1) != len(list3):
+#         raise ValueError("All lists should have the same length.")
+
+#     x_values = range(len(list1))  # Assuming the x-axis represents indices of the lists.
+
+#     plt.plot(x_values, list1, label='baseline')
+#     plt.plot(x_values, list2, label='50%  of baseline')
+#     plt.plot(x_values, list3, label='25%  of baseline')
+
+#     plt.xlabel('Training Steps')
+#     plt.ylabel('Loss')
+#     plt.title('Plot of effects of learning rate on RND')
+#     plt.legend()
+#     plt.grid(True)
+#     plt.show()
+
+
+# plot_three_lists(learning_rates[0], learning_rates[1], learning_rates[2])
---
+++ src/HUSKY_RL/SAC_Husky.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+
+import rospy
+import os
+import numpy as np
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from collections import deque
+from std_msgs.msg import Float32MultiArray
+from src.HUSKY_RL.Husky_ENV import Env
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+from src.HUSKY_RL.Husky_RNDUtils import RNDTauSetter
+import torch
+import numpy as np
+from src.HUSKY_RL.Husky_Networks import CustomNetwork
+from src.HUSKY_RL.Husky_Memory import ReplayBuffer
+import os
+
+
+class SACAgent:
+
+    ALPHA_INITIAL = 1.
+    REPLAY_BUFFER_BATCH_SIZE = 100
+    DISCOUNT_RATE = 0.99
+    LEARNING_RATE = 10 ** -4
+    SOFT_UPDATE_INTERPOLATION_FACTOR = 0.01
+
+    def __init__(self,environment,rnd):
+        self.rnd_state_flag=False
+        self.rnd_state_only=False
+        self.environment = environment
+        self.state_dim = self.environment.observation_size
+        self.action_dim = self.environment.action_size
+        self.critic_local = CustomNetwork(None,True,self.state_dim,
+                                          self.action_dim,"dense_net",None)
+
+        self.critic_local2 = CustomNetwork(None,True,self.state_dim,
+                                          self.action_dim,"dense_net",None)
+
+        self.critic_optimiser = torch.optim.Adam(self.critic_local.parameters(), lr=self.LEARNING_RATE)
+        self.critic_optimiser2 = torch.optim.Adam(self.critic_local2.parameters(), lr=self.LEARNING_RATE)
+
+        self.critic_target = CustomNetwork(None,True,self.state_dim,
+                                          self.action_dim,"dense_net",None)
+
+        self.critic_target2 =CustomNetwork(None,True,self.state_dim,
+                                          self.action_dim,"dense_net",None)
+
+
+        self.soft_update_target_networks(tau=1.)
+
+        self.actor_local = CustomNetwork(None,True,self.state_dim,
+                                          self.action_dim,"dense_net",'softmax')
+        
+        self.actor_optimiser = torch.optim.Adam(self.actor_local.parameters(), lr=self.LEARNING_RATE)
+
+        self.replay_buffer = ReplayBuffer(self.environment)
+        self.rnd_flag=rnd
+        if rnd:
+            self._set_rnd()
+        self.target_entropy = 0.98 * -np.log(1 / self.environment.action_size)
+        self.log_alpha = torch.tensor(np.log(self.ALPHA_INITIAL), requires_grad=True)
+        self.alpha = self.log_alpha
+        self.alpha_optimiser = torch.optim.Adam([self.log_alpha], lr=self.LEARNING_RATE)
+    
+    def _set_rnd(self):
+        output=self.critic_local.feat_foward(torch.rand((64,self.state_dim)))
+        input_dim=2*(output.shape[1])
+        loss_type="MAPE"
+        input_dim2=2*(self.state_dim)
+        self.tausetter=RNDTauSetter(input_dim,loss_type,initial_tau=.003)
+        self.tausetter2=RNDTauSetter(input_dim2,loss_type,initial_tau=.003)
+    def get_next_action(self, state, evaluation_episode=False):
+        if evaluation_episode:
+            discrete_action = self.get_action_deterministically(state)
+        else:
+            discrete_action = self.get_action_nondeterministically(state)
+        return discrete_action
+
+    def get_action_nondeterministically(self, state):
+        action_probabilities = self.get_action_probabilities(state)
+        discrete_action = np.random.choice(range(self.action_dim), p=action_probabilities)
+        return discrete_action
+
+    def get_action_deterministically(self, state):
+        action_probabilities = self.get_action_probabilities(state)
+        discrete_action = np.argmax(action_probabilities)
+        return discrete_action
+    
+    def train_on_transition(self, state, discrete_action, next_state, reward, done):
+        transition = (state, discrete_action, reward, next_state, done)
+        g_loss,l_loss,error=self.train_networks(transition)
+        return g_loss,l_loss,error
+    
+    def train_networks(self, transition):
+        # Set all the gradients stored in the optimisers to zero.
+        self.critic_optimiser.zero_grad()
+        self.critic_optimiser2.zero_grad()
+        self.actor_optimiser.zero_grad()
+        self.alpha_optimiser.zero_grad()
+        # Calculate the loss for this transition.
+        self.replay_buffer.add_transition(transition)
+        # Compute the gradients based on this loss, i.e. the gradients of the loss with respect to the Q-network
+        # parameters.
+        if self.replay_buffer.get_size() >= self.REPLAY_BUFFER_BATCH_SIZE:
+            # get minibatch of 100 transitions from replay buffer
+            minibatch = self.replay_buffer.sample_minibatch(self.REPLAY_BUFFER_BATCH_SIZE)
+            local_minibatch=self.replay_buffer.sample_minibatch(self.REPLAY_BUFFER_BATCH_SIZE)
+            minibatch_separated = list(map(list, zip(*minibatch)))
+            local_minibatch_separated = list(map(list, zip(*local_minibatch)))
+
+            # unravel transitions to get states, actions, rewards and next states
+            states_tensor = torch.tensor(np.array(minibatch_separated[0]))
+            actions_tensor = torch.tensor(np.array(minibatch_separated[1]))
+            rewards_tensor = torch.tensor(np.array(minibatch_separated[2])).float()
+            next_states_tensor = torch.tensor(np.array(minibatch_separated[3]))
+            done_tensor = torch.tensor(np.array(minibatch_separated[4]))
+            
+            states_tensor_local = torch.tensor(np.array(local_minibatch_separated[0]))
+            next_states_tensor_local = torch.tensor(np.array(local_minibatch_separated[3]))
+            
+            
+            critic_loss, critic2_loss = \
+                self.critic_loss(states_tensor, actions_tensor, rewards_tensor, next_states_tensor, done_tensor)
+            
+            if self.rnd_flag:
+                
+                if self.rnd_state_flag:
+                    global_loss,local_loss,error,global_state_loss,local_state_loss,error_state=self.rnd_loss_states(states_tensor,next_states_tensor,
+                                                          states_tensor_local,next_states_tensor_local)
+                elif self.rnd_state_only:
+                    global_loss,local_loss,error=self.rnd_state_loss(states_tensor,next_states_tensor,
+                                                          states_tensor_local,next_states_tensor_local)
+                
+                else:
+                    global_loss,local_loss,error=self.rnd_loss(states_tensor,next_states_tensor,
+                                                          states_tensor_local,next_states_tensor_local)
+                
+
+            
+            critic_loss.backward()
+            critic2_loss.backward()
+            self.critic_optimiser.step()
+            self.critic_optimiser2.step()
+
+            actor_loss, log_action_probabilities = self.actor_loss(states_tensor)
+
+            actor_loss.backward()
+            self.actor_optimiser.step()
+
+            alpha_loss = self.temperature_loss(log_action_probabilities)
+
+            alpha_loss.backward()
+            self.alpha_optimiser.step()
+            self.alpha = self.log_alpha.exp()
+
+            self.soft_update_target_networks()
+            
+            if self.rnd_state_flag:
+                return global_loss,local_loss,error,global_state_loss,local_state_loss,error_state
+            return global_loss,local_loss,error
+        return 0,0,0
+
+    def critic_loss(self, states_tensor, actions_tensor, rewards_tensor, next_states_tensor, done_tensor):
+        with torch.no_grad():
+            action_probabilities, log_action_probabilities = self.get_action_info(next_states_tensor)
+            next_q_values_target = self.critic_target.forward(next_states_tensor)
+            next_q_values_target2 = self.critic_target2.forward(next_states_tensor)
+            soft_state_values = (action_probabilities * (
+                    torch.min(next_q_values_target, next_q_values_target2) - self.alpha * log_action_probabilities
+            )).sum(dim=1)
+
+            next_q_values = rewards_tensor + ~done_tensor * self.DISCOUNT_RATE*soft_state_values
+
+        soft_q_values = self.critic_local(states_tensor).gather(1, actions_tensor.unsqueeze(-1)).squeeze(-1)
+        soft_q_values2 = self.critic_local2(states_tensor).gather(1, actions_tensor.unsqueeze(-1)).squeeze(-1)
+        critic_square_error = torch.nn.MSELoss(reduction="none")(soft_q_values, next_q_values)
+        critic2_square_error = torch.nn.MSELoss(reduction="none")(soft_q_values2, next_q_values)
+        weight_update = [min(l1.item(), l2.item()) for l1, l2 in zip(critic_square_error, critic2_square_error)]
+        self.replay_buffer.update_weights(weight_update)
+        critic_loss = critic_square_error.mean()
+        critic2_loss = critic2_square_error.mean()
+        return critic_loss, critic2_loss
+    
+    def rnd_state_loss(self, states_tensor,states_local_tensor,next_states_tensor,next_states_tensor_local):
+        with torch.no_grad():
+    
+            encoding_global=torch.cat((states_tensor, next_states_tensor), 1)
+            encoding_local=torch.cat((states_local_tensor, next_states_tensor_local), 1)
+            
+        global_loss,local_loss,error=self.tausetter2.update_rnd(encoding_global,encoding_local)
+        return global_loss,local_loss,error
+    
+    def rnd_loss(self, states_tensor,states_local_tensor,next_states_tensor,next_states_tensor_local):
+        with torch.no_grad():
+            encoding_global= self.critic_local.feat_foward(states_tensor)
+            encoding_local = self.critic_local.feat_foward(states_local_tensor)
+
+            encoding_global_next = self.critic_local.feat_foward(next_states_tensor_local)
+            encoding_local_next = self.critic_local.feat_foward(next_states_tensor_local)
+            
+            encoding_global=torch.cat((encoding_global, encoding_global_next), 1)
+            encoding_local=torch.cat((encoding_local, encoding_local_next), 1)
+            
+        global_loss,local_loss,error=self.tausetter.update_rnd(encoding_global,encoding_local)
+        
+        return global_loss,local_loss,error
+    
+    def rnd_only_state_loss(self, states_tensor,states_local_tensor,next_states_tensor,next_states_tensor_local):
+        with torch.no_grad():
+            encoding_global= self.critic_local.feat_foward(states_tensor)
+            encoding_local = self.critic_local.feat_foward(states_local_tensor)
+
+            encoding_global_next = self.critic_local.feat_foward(next_states_tensor_local)
+            encoding_local_next = self.critic_local.feat_foward(next_states_tensor_local)
+            
+            encoding_global=torch.cat((encoding_global, encoding_global_next), 1)
+            encoding_local=torch.cat((encoding_local, encoding_local_next), 1)
+            
+        global_loss,local_loss,error=self.tausetter2.update_rnd(encoding_global,encoding_local)
+        
+        return global_loss,local_loss,error
+        
+
+    def rnd_loss_states(self, states_tensor,states_local_tensor,next_states_tensor,next_states_tensor_local):
+        with torch.no_grad():
+            encoding_global= self.critic_local.feat_foward(states_tensor)
+            encoding_local = self.critic_local.feat_foward(states_local_tensor)
+
+            encoding_global_next = self.critic_local.feat_foward(next_states_tensor_local)
+            encoding_local_next = self.critic_local.feat_foward(next_states_tensor_local)
+            
+            encoding_global=torch.cat((encoding_global, encoding_global_next), 1)
+            encoding_local=torch.cat((encoding_local, encoding_local_next), 1)
+            
+            encoding_global_states=torch.cat((states_tensor, next_states_tensor), 1)
+            encoding_local_states=torch.cat((states_local_tensor, next_states_tensor_local), 1)
+            
+        
+        global_state_loss,local_state_loss,error_state=self.tausetter2.update_rnd(encoding_global_states, encoding_local_states)
+        global_loss,local_loss,error=self.tausetter.update_rnd(encoding_global,encoding_local)
+        
+        return global_loss,local_loss,error,global_state_loss,local_state_loss,error_state
+    
+    def actor_loss(self, states_tensor):
+        action_probabilities, log_action_probabilities = self.get_action_info(states_tensor)
+        q_values_local = self.critic_local(states_tensor)
+        q_values_local2 = self.critic_local2(states_tensor)
+        inside_term = self.alpha * log_action_probabilities - torch.min(q_values_local, q_values_local2)
+        policy_loss = (action_probabilities * inside_term).sum(dim=1).mean()
+        return policy_loss, log_action_probabilities
+
+    def temperature_loss(self, log_action_probabilities):
+        alpha_loss = -(self.log_alpha * (log_action_probabilities + self.target_entropy).detach()).mean()
+        return alpha_loss
+
+    def get_action_info(self, states_tensor):
+        action_probabilities = self.actor_local.forward(states_tensor)
+        z = action_probabilities == 0.0
+        z = z.float() * 1e-8
+        log_action_probabilities = torch.log(action_probabilities + z)
+        return action_probabilities, log_action_probabilities
+
+    def get_action_probabilities(self, state):
+        state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
+        action_probabilities = self.actor_local.forward(state_tensor)
+        return action_probabilities.squeeze(0).detach().numpy()
+
+    def soft_update_target_networks(self, tau=SOFT_UPDATE_INTERPOLATION_FACTOR):
+        self.soft_update(self.critic_target, self.critic_local, tau)
+        self.soft_update(self.critic_target2, self.critic_local2, tau)
+
+    def soft_update(self, target_model, origin_model, tau):
+        for target_param, local_param in zip(target_model.parameters(), origin_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1 - tau) * target_param.data)
+
+    def predict_q_values(self, state):
+        q_values = self.critic_local(state)
+        q_values2 = self.critic_local2(state)
+        return torch.min(q_values, q_values2)
+    
+    def save_models(self,file_path):
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+            
+        torch.save(self.critic_local.state_dict(), file_path+"\\critic_local")
+        torch.save(self.critic_local2.state_dict(), file_path+"\\critic_local2")
+        torch.save(self.actor_local.state_dict(), file_path+"\\actor")
+        torch.save(self.critic_target.state_dict(), file_path+"\\t_critic_local")
+        torch.save(self.critic_target2.state_dict(), file_path+"\\t_critic_local2")
+        
+    def load_models(self,file_path):
+        self.critic_local.load_state_dict(torch.load(file_path+"\\critic_local"))
+        self.critic_local2.load_state_dict(torch.load(file_path+"\\critic_local2"))
+        self.actor_local.load_state_dict(torch.load(file_path+"\\actor"))
+        self.critic_target.load_state_dict(torch.load(file_path+"\\t_critic_local"))
+        self.critic_target2.load_state_dict(torch.load(file_path+"\\t_critic_local2"))
+        
+        self.critic_local.eval()  
+        self.critic_local2.eval()
+        self.actor_local.eval()
+        
+        
+    def save_state(self,path):
+        self.replay_buffer.save_state(path)
+        self.save_models(path)
+        
+    def load_state(self,path):
+        self.replay_buffer.load_state(path+"\\agents_data")
+        self.load_models(path)        
